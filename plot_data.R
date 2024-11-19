fac15f58e56bd9fded6dd7205699128af9069493
flamingobadger <rebecca.mccash@pmb.ox.ac.uk>
2024-11-19 16:15:32 +0000

    Update plot_data.R

--- plot_data.R
+++ plot_data.R
@@ -5,7 +5,7 @@ growth_data <- read.csv("experiment.csv")
 install.packages("ggplot2")
 library(ggplot2)
 
-ggplot(aes(t,N), data = "experiment.csv") +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_point() +
   
